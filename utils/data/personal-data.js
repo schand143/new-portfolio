6731026d89c7dad35866105ed1ead18e17b1259c
schand143 <shaikhchand724@gmail.com>
2025-04-17 22:36:19 +0530

    initial commit

---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "CHAND SHAIKH",
+  profile: '/profile.png',
+  designation: "Sr. Application Developer",
+  description: "My name is CHAND SHAIKH. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'shaikhchand724@gmail.com',
+  phone: '+919823806191',
+  address: 'Pune, Maharashtra, India - 411048 ',
+  github: 'https://github.com/schand143/',
+  facebook: 'https://www.facebook.com/abusaid.riyaz/',
+  linkedIn: 'https://www.linkedin.com/in/chand143/',
+  twitter: 'https://twitter.com/said7388',
+  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+  leetcode: "https://leetcode.com/said3812/",
+  devUsername: "chand123",
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+}
