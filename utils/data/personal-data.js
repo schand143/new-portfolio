f460a11ada9e74920d3ffdef587bd3c4e0c17a91
schand143 <shaikhchand724@gmail.com>
2025-05-02 17:14:09 +0530

    resume updated

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "CHAND SHAIKH",
   profile: '/profile.png',
   designation: "Sr. Application Developer",
-  description: "My name is CHAND SHAIKH. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "Hi, I'm Chand Shaikh — a passionate and results-driven Frontend Developer. I'm an enthusiastic programmer with a strong self-learning mindset and a love for exploring new technologies. My core expertise lies in JavaScript and its powerful ecosystem, including React, Next.js, and Angular. I thrive on building engaging, user-friendly web applications and am deeply committed to making the web more open, accessible, and innovative. Always eager to tackle new challenges, I’m excited to collaborate on projects that push boundaries and drive meaningful impact.",
   email: 'shaikhchand724@gmail.com',
   phone: '+91-9823806191',
   address: 'Pune, Maharashtra, India - 411048 ',
@@ -13,5 +13,5 @@ export const personalData = {
   stackOverflow: 'https://stackoverflow.com/users/9256603/chand-shaikh',
   leetcode: "https://leetcode.com/said3812/",
   devUsername: "schand143",
-  resume: "https://drive.google.com/file/d/1WoNIfu1tRcex-TewnDwY5lF4jXt0pLO6/view"
+  resume: "https://drive.google.com/file/d/14eulTUslqSguN6KjpzJOsPbW-GLKkHIL/view?usp=drive_link"
 }
